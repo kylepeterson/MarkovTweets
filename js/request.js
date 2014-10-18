1169f051c0415272ccb8ee812da58da11c222744
Kyle Peterson <peterson.kyle9@gmail.com>
2014-10-18 02:14:19 -0700

    populatin the grid round 2

--- js/request.js
+++ js/request.js
@@ -1,4 +1,6 @@
+// Contains positions with tweets in them
 var grid = new Array();
+// Tweets that are placed and have yet to be expanded fully
 var unvisitedTweets = new Array();
 
 $(function() {
@@ -10,31 +12,43 @@ $(function() {
 	})
 })
 
+// Populates the page with tweets
 function populateTweets(response) {
 	var firstTweet = chooseRandom(response);
-	populateFirstTweet(response);
+	// Place initial tweet in upper left corner
+	placeTweet(firstTweet, [0, 0]);
+	grid.append([0, 0]);
+	firstTweet.expansions = 1;
 	unvisitedTweets.push(chooseRandom(response));
 	while (unvisitedTweets.length > 0);
+		// Choose random tweet that is unexpanded
 		var currentTweet = chooseRandom(unvisitedTweets);
-		unvisitedTweets.remove(currentTweet);
-		var expandPosition = checkPosition(currentTweet);
+		// Check position for validity
+		var expandPosition = checkForPosition(currentTweet);
 		if(expandPosition != -1) {
-			// Place node in expandPosition...
+			// If a valid random position was found expand to it
+			currentTweet.expansions++;
+			placeTweet(currentTweet, [expandPosition]);
+		}
+		// If the tweet has expanded twice remove it from consideration for expansion
+		if(currentTweet.expansions >= 2) {
+			unvisitedTweets.remove(currentTweet);
 		}
 	}
 }
 
-function populateFirstTweet(tweet) {
-	tweet.x = 0;
-	tweet.y = 0;
-	/// Place node in 0, 0 position
-}
-
+// Places the given tweet in the given position
 function placeTweet(tweet, position) {
-	var instance = $('.tweet .template')
+	var instance = $('.tweet .template').clone();
+	instance.find('.user-pic').src = tweet.picture_url;
+	instance.find('.user-name').html = tweet.author;
+	instance.find('.tweet-text').html = tweet.text;
+	grid(position) = true;
 }
 
-function checkPosition(tweet) {
+// Checks if there is a valid position around the given tweet
+// Returns random valid position if one exists and returns -1 if none exist.
+function checkForPosition(tweet) {
 	var potentialPositions = new Array();
 	potentialPositions.push([tweet.x, tweet.y - 1]);
 	potentialPositions.push([tweet.x, tweet.y + 1]);
@@ -43,12 +57,20 @@ function checkPosition(tweet) {
 	var start = Math.random() * 4;
 	for(i = 0; i < 4; i++) {
 		var cur = potentialPositions[start % potentialPositions.length];
-		if(grid[cur] != null)
+		if(validPos(cur)) {
 			return cur;
+		}
 	}
 	return -1;
 }
 
+// Return true if a tweet can be placed at this position
+function validPos(pos) {
+	return $.inArray(pos, grid) 
+		&& pos[1] < 3 && pos[1] >= 0 && pos[0] >= 0;
+}
+
+// Chooses a random element from the given list
 function chooseRandom(list) {
 	return list[Math.floor(Math.random() * list.length)];
 }
